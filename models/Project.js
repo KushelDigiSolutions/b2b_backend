6df73710336aec218e2477121bb7dc5f7a1c1a60
Manishrajwar <manishsinghrajwar80@gmail.com>
2024-02-10 22:24:09 +0530

    this

--- models/Project.js
+++ models/Project.js
@@ -4,10 +4,31 @@ const mySchema = mongoose.Schema({
     title: String,
     location: String,
     desc: String,
+    bidDate:{
+        type: String,
+        
+    },
+    startDate:{
+        type: String,
+        
+    },
+    stage:{
+        type:String ,
+    }
+    ,
+    buildingUse:{
+       type: String ,
+    },
+    sector: String ,
+    value: Number , 
     defaultImg: String,
     createdBy: Object,
     ts: String,
     status: String,
+    folder: {
+        type :String ,
+    
+    },
     img: {
         type: Array,
         default: []
